1939bbc743cd9c45cb125ec1f658fa3aa7929570
probablynotval <162844653+probablynotval@users.noreply.github.com>
2024-07-09 18:21:22 +0000

    chore: move shuffle into main, cleanup main

--- src/commands/next.rs
+++ src/commands/next.rs
@@ -1,42 +1,5 @@
 // TODO: Add next function
-// FIXME: ??? if images are added then what ???
-
-use log::{debug, error, info};
-use rand::{seq::SliceRandom, thread_rng};
-use std::{fs, process::Command};
-use walkdir::WalkDir;
-
-use crate::get_current;
-
-// TODO: Probably move this function to main.rs since it's a fundamental part of the program
-pub fn shuffle(interval: &Option<u32>) -> Result<(), std::io::Error> {
-    // Get wallpaper directory
-    let file_path = get_current().unwrap();
-    // Remove file from directory path
-    let current = file_path.parent().unwrap();
-
-    // Create vector to hold paths
-    let mut paths = Vec::new();
-
-    // Recursively search directory for wallpapers
-    for entry in WalkDir::new(current).follow_links(true) {
-        // Assert that entry is DirEntry and not error
-        let entry = entry?;
-        // If the path is valid push it to the vector (what would be an invalid path? is it possible?)
-        if entry.path().is_file() {
-            if let Some(path) = entry.path().to_str() {
-                paths.push(path.to_string());
-            }
-        }
-    }
-
-    let mut rng = thread_rng();
-    paths.shuffle(&mut rng);
-    debug!("{paths:#?}");
-
-    Ok(())
-}
 
 pub fn next() {
-    println!("WIP");
+    println!("WIP")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod commands;
 
-use commands::{favorite::favorite, next::shuffle};
+use commands::favorite::favorite;
 
 use clap::{Parser, Subcommand};
 use env_logger::{Builder, Target};
-use log::{debug, LevelFilter};
+use log::LevelFilter;
+use rand::{seq::SliceRandom, thread_rng};
 use regex::Regex;
 use std::{path::PathBuf, process::Command, str};
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[command(name = "WallFlick")]
@@ -48,6 +50,33 @@ fn get_current() -> anyhow::Result<PathBuf> {
     }
 
     Err(anyhow::anyhow!("[swww query] No valid path found"))
+}
+
+pub fn shuffle(interval: &Option<u32>) -> Result<Vec<String>, std::io::Error> {
+    // Get wallpaper directory
+    let file_path = get_current().unwrap();
+    // Remove file from directory path
+    let current = file_path.parent().unwrap();
+
+    // Create vector to hold paths
+    let mut paths = Vec::new();
+
+    // Recursively search directory for wallpapers
+    for entry in WalkDir::new(current).follow_links(true) {
+        // Assert that entry is DirEntry and not error
+        let entry = entry?;
+        // If the path is valid push it to the vector (what would be an invalid path? is it possible?)
+        if entry.path().is_file() {
+            if let Some(path) = entry.path().to_str() {
+                paths.push(path.to_string());
+            }
+        }
+    }
+
+    let mut rng = thread_rng();
+    paths.shuffle(&mut rng);
+
+    Ok(paths)
 }
 
 fn main() {
