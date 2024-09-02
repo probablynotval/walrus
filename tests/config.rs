4d4ae0d9927c99fd5ef575c8e0d4cb9d12ee0ca2
probablynotval <162844653+probablynotval@users.noreply.github.com>
2024-09-02 02:56:04 +0000

    test(config): fix test

--- tests/config.rs
+++ tests/config.rs
@@ -1,8 +1,7 @@
 use walrus::config::*;
 
-use std::error::Error;
-use std::fs::File;
-use std::io::Write;
+use directories::UserDirs;
+use std::{error::Error, fs::File, io::Write, path::PathBuf};
 use tempfile::tempdir;
 
 #[test]
@@ -32,16 +31,39 @@ fn test_config_from() -> Result<(), Box<dyn Error>> {
 
     assert_eq!(
         *config.general.as_ref().unwrap().path.as_ref().unwrap(),
-        String::from("$HOME/Pictures")
+        PathBuf::from("$HOME/Pictures")
     );
     assert_eq!(
         *config.general.as_ref().unwrap().interval.as_ref().unwrap(),
-        60 as usize
+        60 as u64
     );
     assert_eq!(
         *config.transition.as_ref().unwrap().fps.as_ref().unwrap(),
-        180 as usize
+        180 as u32
     );
+
+    Ok(())
+}
+
+#[test]
+fn test_config_from_defaults() -> Result<(), Box<dyn Error>> {
+    let config = Config::default();
+    let general = config.clone().general.unwrap_or_default();
+    let path = general.path.unwrap_or_default();
+    let interval = general.interval.unwrap_or_default();
+    let transition = config.clone().transition.unwrap_or_default();
+    let fps = transition.fps.unwrap_or_default();
+
+    assert_eq!(
+        path,
+        UserDirs::new()
+            .unwrap()
+            .picture_dir()
+            .unwrap()
+            .join("Wallpapers")
+    );
+    assert_eq!(interval, 60 as u64);
+    assert_eq!(fps, 180 as u32);
 
     Ok(())
 }
